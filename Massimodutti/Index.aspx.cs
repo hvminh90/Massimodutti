326401c99aa68b483c4a6b18fa744eca23bd9885
binhhv <hvbinh1990@gmail.com>
2015-07-02 09:26:27 +0700

    fix login. binhhv

--- Massimodutti/Index.aspx.cs
+++ Massimodutti/Index.aspx.cs
@@ -11,7 +11,10 @@ namespace Massimodutti
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //if (Session["username"] != null)
+            //{
 
+            //}
         }
     }
 }
